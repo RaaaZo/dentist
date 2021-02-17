da28c890f94cc2370cce1aea9abfbfc5809d948c
Mateusz Koprowicz <koprowicz.mateusz@gmail.com>
2021-02-17 21:39:43 +0100

    adding styles for home page

---
+++ src/data/homeData.ts
@@ -0,0 +1,37 @@
+import { ReactComponent as BuildingIcon } from 'assets/svg/building-vector.svg';
+import { ReactComponent as Building } from 'assets/svg/building.svg';
+import { ReactComponent as Woman } from 'assets/svg/woman.svg';
+import { ReactComponent as Dentist } from 'assets/svg/dentist.svg';
+import { ReactComponent as LocationIcon } from 'assets/svg/location-vector.svg';
+import { ReactComponent as Location } from 'assets/svg/location.svg';
+
+export const listOfTopics = [
+  {
+    id: '0675420c-714b-11eb-9439-0242ac130002',
+
+    icon: Dentist,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam aut animi id nemo obcaecati consequuntur a tempora facere quidem odit.',
+    link: '/usługi',
+    svg: Woman,
+    inverted: false,
+  },
+  {
+    id: '067544a0-714b-11eb-9439-0242ac130002',
+    icon: BuildingIcon,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam aut animi id nemo obcaecati consequuntur a tempora facere quidem odit.',
+    link: '/gabinet',
+    svg: Building,
+    inverted: true,
+  },
+  {
+    id: '06755e5e-714b-11eb-9439-0242ac130002',
+    icon: LocationIcon,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam aut animi id nemo obcaecati consequuntur a tempora facere quidem odit.',
+    link: '/kontakt',
+    svg: Location,
+    inverted: false,
+  },
+];
